eee4b85ef6eec38f23e23df481e2733b44b32d75
Joseph Baylon <joseph@gradientx.com>
2014-04-13 19:15:07 -0700

    initial commit of scopely-challenge answers

--- src/main/java/com/scopely/challenge/builder/StringValueBuilderImpl.java
+++ src/main/java/com/scopely/challenge/builder/StringValueBuilderImpl.java
@@ -4,12 +4,21 @@ public class StringValueBuilderImpl implements ValueBuilder<String, String> {
 
     private StringBuilder builder = new StringBuilder();
 
+    /**
+     *
+     * @param value
+     * @return
+     */
     @Override
     public ValueBuilder<String, String> append(String value) {
         builder.append(value).append("-");
         return this;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public String build() {
         builder.deleteCharAt(builder.lastIndexOf("-"));
--- src/main/java/com/scopely/challenge/builder/ValueBuilder.java
+++ src/main/java/com/scopely/challenge/builder/ValueBuilder.java
@@ -2,7 +2,17 @@ package com.scopely.challenge.builder;
 
 public interface ValueBuilder<ValueType, ReturnType> {
 
+    /**
+     *
+     * @param value
+     * @return
+     */
     public ValueBuilder<ValueType, ReturnType> append(final ValueType value);
+
+    /**
+     *
+     * @return
+     */
     public ReturnType build();
 
 }
